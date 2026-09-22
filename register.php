c0fa1139d1872025a457864cf17cc49d82aee61c
trolge <oofgamersacc@gmail.com>
2026-09-22 12:35:35 +0700

    feat(): added cookie params

--- register.php
+++ register.php
@@ -1,4 +1,8 @@
 <?php
+    session_set_cookie_params([
+        'httponly' => true,
+        'secure' => true
+    ]);
     session_start();
 
     if (!isset($_SESSION['logged_in_user']) && isset($_COOKIE['remember_user'])) {
